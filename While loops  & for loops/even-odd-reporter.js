b781c8a303311cf12e0cef0459fce232303893a0
Jayh01 <84378798+Jayh01@users.noreply.github.com>
2021-09-08 19:20:31 +0200

    Add files via upload

---
+++ While loops  & for loops/even-odd-reporter.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 20; i++) {
+     console.log(`Is ${i} even or odd?`);
+
+        if (i % 20 == 0 ){
+            console.log ('Even')
+        } else{
+            console.log('Oneven')
+        }
+        // TODO: determine if i is odd or even
+        // TODO: use console.log
+}
---
+++ While loops  & for loops/grade-assigner.js
@@ -0,0 +1,22 @@
+// let toets = function (score) {
+//     if (score > 90) {
+//       return "A";
+//     } else if (score > 80) {
+//       return "B";
+//     } else if (score > 70) {
+//       return "C";
+//     } else if (score > 65) {
+//       return "D";
+//     } else  {
+//       return "E";
+//     }
+//   }
+
+
+//   for (let som = 60; som < toets.length; som++) {
+//       const element =  toets[som];
+      
+//   }
+
+//   let uitkomst = toets(82);
+//   console.log(uitkomst)
